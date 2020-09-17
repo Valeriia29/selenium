db018204fde31a7203ae58f52a6a5556a242182c
vkonovki <Valeriia.Konovkina@t-systems.com>
2020-09-17 10:58:14 +0300

    HW 4 - Задание 4. Сделайте сценарий логина         Сделайте сценарий для логина в панель администрирования учебного приложения http://localhost/litecart/admin/.         Рекомендации: приложение должно включать действия входа и выхода в учебном приложении

---
+++ java-example/src/test/java/ru/selenium/training/LoginTest.java
@@ -0,0 +1,57 @@
+package ru.selenium.training;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class LoginTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+
+    @Before
+    public void start(){
+        //System.setProperty("webdriver.chrome.driver", "C:\\workspace\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+
+    @Test
+    public void MyFirstTest()   {
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        //driver.findElement(B)
+        //driver.findElement((By.cssSelector("button")).);
+        driver.findElement(By.cssSelector("button")).click();
+        System.out.println(driver.getTitle());
+        String textlogIn = "Dashboard | My Store";
+        wait.until(titleIs(textlogIn));
+        WebElement classLogOut = driver.findElement(By.xpath("//a[@href='http://localhost/litecart/admin/logout.php']"));
+        classLogOut.click();
+        System.out.println(driver.getTitle());
+        String textLogOut = "My Store";
+        wait.until(titleIs(textLogOut));
+    }
+
+
+
+
+//   // @After
+//   // public void stop(){
+//        driver.quit();
+//        driver = null;
+//    }
+}
